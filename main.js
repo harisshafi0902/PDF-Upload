66a7272fc0070d06cb6a25de111caa19b03f158a
Haris Shafi <114337919+harisshafi0902@users.noreply.github.com>
2023-05-31 13:58:38 +0500

    Updated to Prevent same name PDF File

--- main.js
+++ main.js
@@ -3,48 +3,63 @@ const express = require("express");
 const multer = require("multer");
 const app = express();
 const PORT = 5000;
-
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg" ||
-    file.mimetype === "application/pdf"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-// const upload = multer({ storage: fileStorage, fileFilter: fileFilter });
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    return cb(null, "./uploads");
+    cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
-    return cb(null, `${Date.now()}-${file.originalname}`);
+    const filePath = path.join(__dirname, "uploads", file.originalname);
+
+    // Check if the file already exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        // File does not exist, allow the upload
+        cb(null, file.originalname);
+      } else {
+        // File already exists, reject the upload
+        cb(new Error("File already exists."));
+      }
+    });
   },
 });
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "application/pdf") {
+    cb(null, true);
+  } else {
+    cb(new Error("Only PDF files are allowed."));
+  }
+};
 
 const upload = multer({ storage, fileFilter });
 
-// const upload = multer({ dest: "uploads/" });
-
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
-app.use(express.urlencoded({ entended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  return res.render("homepage");
+  res.render("homepage");
 });
 
-app.post("/upload", upload.single("profileImage"), (req, res) => {
+app.post("/upload", upload.single("profileImage"), (req, res, next) => {
+  console.log("=====>>>", req);
+  if (req.fileExists) {
+    // File already exists
+    return res.status(409).send("File already exists.");
+  }
+
+  if (req.fileExtensionCheck) {
+    // Only PDF files are allowed
+    return res.status(400).send("Only PDF files are allowed.");
+  }
+
   console.log(req.body);
   console.log(req.file);
 
-  return res.redirect("/");
+  res.redirect("/");
 });
 
 app.listen(PORT, () => console.log("Server Started at PORT:5000"));
+
